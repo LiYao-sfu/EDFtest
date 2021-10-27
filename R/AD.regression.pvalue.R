e017e4bcd2aa7e609cbb34d185fda61a1425c5d3
Li <yaoliy@sfu.ca>
2021-10-27 13:20:47 -0400

    Add AD Watson regression

--- R/AD.regression.pvalue.R
+++ R/AD.regression.pvalue.R
@@ -21,7 +21,7 @@ NULL
 AD.normal.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
   p=dim(x)[2]
   n=dim(x)[1]
-  e = CvM.normal.regression.eigen(x,neig=neig)
+  e = AD.normal.regression.eigen(x,neig=neig)
   plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
   warn=getOption("warn")
   im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
@@ -37,3 +37,145 @@ AD.normal.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
   }
   list(P=p,error=aerror)
 }
+
+
+#' @export
+#' @rdname AD.regression.pvalue
+AD.gamma.regression.pvalue = function(w,x,theta,link="log",neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = AD.gamma.regression.eigen(x,theta=theta,link=link,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname AD.regression.pvalue
+AD.logistic.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = AD.logistic.regression.eigen(x,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname AD.regression.pvalue
+AD.laplace.regression.pvalue = function(w,xneig=max(100,n),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = AD.laplace.regression.eigen(w,xneig=max(100,n))
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," and neig = ",neig,
+                      " imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," and neig = ",neig,
+                    " p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname AD.regression.pvalue
+AD.weibull.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = AD.weibull.regression.eigen(x,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname AD.regression.pvalue
+AD.extremevalue.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = AD.weibull.regression.eigen(x,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname AD.regression.pvalue
+AD.exp.regression.pvalue = function(w,x,theta,link="log",neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = AD.exp.regression.eigen(x,theta=theta,link=link,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+# Helpers -----------------------------------------------------------------
+
+
--- R/Watson.regression.pvalue.R
+++ R/Watson.regression.pvalue.R
@@ -21,7 +21,7 @@ NULL
 Watson.normal.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
   p=dim(x)[2]
   n=dim(x)[1]
-  e = CvM.normal.regression.eigen(x,neig=neig)
+  e = Watson.normal.regression.eigen(x,neig=neig)
   plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
   warn=getOption("warn")
   im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
@@ -37,3 +37,145 @@ Watson.normal.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
   }
   list(P=p,error=aerror)
 }
+
+
+#' @export
+#' @rdname Watson.regression.pvalue
+Watson.gamma.regression.pvalue = function(w,x,theta,link="log",neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = Watson.gamma.regression.eigen(x,theta=theta,link=link,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname Watson.regression.pvalue
+Watson.logistic.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = Watson.logistic.regression.eigen(x,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname Watson.regression.pvalue
+Watson.laplace.regression.pvalue = function(w,xneig=max(100,n),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = Watson.laplace.regression.eigen(w,xneig=max(100,n))
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," and neig = ",neig,
+                      " imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," and neig = ",neig,
+                    " p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname Watson.regression.pvalue
+Watson.weibull.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = Watson.weibull.regression.eigen(x,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname Watson.regression.pvalue
+Watson.extremevalue.regression.pvalue = function(w,x,neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = Watson.weibull.regression.eigen(x,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+
+#' @export
+#' @rdname Watson.regression.pvalue
+Watson.exp.regression.pvalue = function(w,x,theta,link="log",neig=max(n,100),verbose=FALSE){
+  p=dim(x)[2]
+  n=dim(x)[1]
+  e = Watson.exp.regression.eigen(x,theta=theta,link=link,neig=neig)
+  plb=pchisq(w/max(e),df=1,lower.tail = FALSE)
+  warn=getOption("warn")
+  im = imhof(w,lambda=e,epsabs = 1e-9,limit=2^7)
+  options(warn=warn)
+  aerror=im$abserr
+  p=im$Qq
+  if(p<0&&verbose)cat("for W = ",w," using = ",neig,
+                      " eigenvalues, imhof returned a negative probability\n")
+  if(p<plb){
+    p=plb
+    if(verbose) cat("for W = ",w," using = ",neig,
+                    " eigenvalues, p was replaced by a lower bound on p: ",plb, "\n")
+  }
+  list(P=p,error=aerror)
+}
+
+# Helpers -----------------------------------------------------------------
+
+
