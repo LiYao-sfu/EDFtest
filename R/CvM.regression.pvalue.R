6357afd1b5baa4d67be5d9fb69e714d0d1ee1339
Richardlockhart <lockhart@sfu.ca>
2021-11-22 16:05:23 -0800

    Update CvM.regression.pvalue.R

--- R/CvM.regression.pvalue.R
+++ R/CvM.regression.pvalue.R
@@ -347,9 +347,9 @@ CvM.logistic.regression.covmat=function(x,neig=max(100,n)){
   M1=outer(s,s,pmin)-outer(s,s)
   G1 = s*(1-s)
   G2 = G1*(log(s/(1-s)))
-  Del = apply(x,2,mean) # Del should now have length p+1
-  G1 = outer(x,G1) # G1 should now have dimension p by neig
-  M2 = cbind(G1,G2) # G1 should now have dimension p+1 by neig
+  Del = apply(x,2,mean) # Del should now have length p
+  G1 = outer(Del,G1) # G1 should now have dimension p by neig
+  M2 = cbind(G1,G2) # M2 should now have dimension p+1 by neig
   M1-t(M2)%*%solve(Fisher,M2)
 }
 
@@ -379,8 +379,8 @@ CvM.laplace.regression.covmat=function(x,neig = max(n,100)){
   G2 = -s*log(2*s)
   G2[s>0.5] = ((1-s)*log(2*(1-s)))[s>0.5]
   Del = apply(x,2,mean) # Del should now have length p
-  G1 = outer(x,G1) # G1 should now have dimension p by neig
-  M2 = cbind(G1,G2) # G1 should now have dimension p+1 by neig
+  G1 = outer(Del,G1) # G1 should now have dimension p by neig
+  M2 = cbind(G1,G2) # M2 should now have dimension p+1 by neig
   M1 - t(M2) %*% solve(Fisher,M2)
 }
 
